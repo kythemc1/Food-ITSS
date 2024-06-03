09cde14631b969060803d99c68591551b869c291
kainrii <117970207+lydk-0326@users.noreply.github.com>
2024-06-03 15:36:14 +0700

    refactor home/about

--- src/components/Pages/Home/About/About.js
+++ src/components/Pages/Home/About/About.js
@@ -3,19 +3,19 @@ import './About.css'
 
 const About = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-14 m-16'>
-            <div className="card bg-base-100 shadow-xl items-center" style={{ backgroundColor: `#efefed`}}>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-14 p-16 bg-white'>
+            <div className="card bg-base-100 shadow-xl items-center" style={{ backgroundColor: `rgb(244 244 245)`}}>
                 <div className="card-body w-full md:w-10/12">
-                    <h2 className="card-title mx-auto text-2xl mb-3">Are You A Local Restaurant?</h2>
+                    <h2 className="card-title mx-auto text-2xl mb-3 text-black">Are You A Local Restaurant?</h2>
                     <p className='text-center mb-5'>Connect With More Clients</p>
                     <div className="card-actions justify-center">
-                    <button className="w-full btn">Join Food Monster today</button>
+                    <button className="w-full btn ">Join Food Monster today</button>
                     </div>
                 </div>
             </div>
-            <div className="card bg-base-100 shadow-xl items-center" style={{ backgroundColor: `#efefed`}}>
+            <div className="card bg-base-100 shadow-xl items-center" style={{ backgroundColor: `rgb(244 244 245)`}}>
                 <div className="card-body w-full md:w-10/12">
-                    <h2 className="card-title mx-auto text-2xl mb-3">About Food Monster</h2>
+                    <h2 className="card-title mx-auto text-2xl mb-3 text-black">About Food Monster</h2>
                     <p className='text-center mb-5'>CFinding Local Restaurants is easy with eats-usa. Search our website to instantly connect with Local Restaurants.</p>
                     <div className="card-actions justify-center">
                     <button className="w-full btn">Get listed today</button>
--- src/components/Pages/Home/Features/Features.js
+++ src/components/Pages/Home/Features/Features.js
@@ -4,7 +4,7 @@ import { RiWechatLine } from "react-icons/ri";
 
 const Features = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-3 py-12' style={{ backgroundColor: `#F7F7F7`}}>
+        <div className='grid grid-cols-1 md:grid-cols-3 py-12' style={{ backgroundColor: `rgb(244 244 245)`}}>
            <div className='text-center mx-auto mb-5 md:mb-0'>
                 <FaSearch className='mx-auto w-10 h-10 mb-4 color-red'/>
                 <h4 className='text-2xl font-bold mb-1 text-black'>Search</h4>
