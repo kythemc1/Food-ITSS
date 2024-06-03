ddab68c4810807faf6b9d8816e1e17240ccbce43
kainrii <117970207+lydk-0326@users.noreply.github.com>
2024-06-03 15:28:15 +0700

    refactor home/feature

--- src/components/Pages/Home/Features/Features.js
+++ src/components/Pages/Home/Features/Features.js
@@ -7,17 +7,17 @@ const Features = () => {
         <div className='grid grid-cols-1 md:grid-cols-3 py-12' style={{ backgroundColor: `#F7F7F7`}}>
            <div className='text-center mx-auto mb-5 md:mb-0'>
                 <FaSearch className='mx-auto w-10 h-10 mb-4 color-red'/>
-                <h4 className='text-2xl font-bold mb-1'>Search</h4>
+                <h4 className='text-2xl font-bold mb-1 text-black'>Search</h4>
                 <p>Search Restaurants</p>
             </div> 
            <div className='text-center mx-auto mb-5 md:mb-0'>
                 <FaCheckCircle className='mx-auto w-10 h-10 mb-4 color-red'/>
-                <h4 className='text-2xl font-bold mb-1'>Review</h4>
+                <h4 className='text-2xl font-bold mb-1 text-black'>Review</h4>
                 <p>Compare Restaurants You Find</p>
             </div> 
            <div className='text-center mx-auto mb-5 md:mb-0'>
                 <RiWechatLine className='mx-auto w-10 h-10 mb-4 color-red'/>
-                <h4 className='text-2xl font-bold mb-1'>Connect</h4>
+                <h4 className='text-2xl font-bold mb-1 text-black'>Connect</h4>
                 <p>Contact Restaurants You Like</p>
             </div> 
         </div>
