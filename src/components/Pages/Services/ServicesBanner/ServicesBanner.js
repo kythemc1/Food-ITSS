c5f0af5735b08c5de1ba66ea5ba3859f693df158
Do Chi Thanh <chithanhdo2002@gmail.com>
2024-06-05 16:56:13 +0700

    Feat: Handle Searchbar query

--- src/components/Pages/Services/ServicesBanner/ServicesBanner.js
+++ src/components/Pages/Services/ServicesBanner/ServicesBanner.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import BgImage from '../../../../assets/images/u594-hero.jpg';
+import React from "react";
+import BgImage from "../../../../assets/images/u594-hero.jpg";
 
-const ServicesBanner = () => {
-    return (
-        <div className="hero" style={{ backgroundImage: `url("${BgImage}")` }}>
-            <div className="hero-overlay bg-opacity-70"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="my-12">
-                    <h1 className="mb-5 text-5xl font-bold text-white">Search Your Favorite Food</h1>
-                    <p className="mb-5">Find restaurants for free</p>
-                    <div className="form-control flex flex-row">
-                        <div className='mr-1 w-9/12 '>
-                            <input type="search" placeholder="Search…" className="input w-full text-black bg-white" />
-                        </div>
-                        <button className="btn w-3/12 color-red">
-                        Search
-                        </button>
-                    </div>
-                </div>
+const ServicesBanner = ({ search, setSearch }) => {
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className="hero" style={{ backgroundImage: `url("${BgImage}")` }}>
+      <div className="hero-overlay bg-opacity-70"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="my-12">
+          <h1 className="mb-5 text-5xl font-bold text-white">
+            Search Your Favorite Food
+          </h1>
+          <p className="mb-5">Find restaurants for free</p>
+          <div className="form-control flex flex-row">
+            <div className="mr-1 w-9/12 ">
+              <input
+                type="search"
+                placeholder="Search…"
+                className="input w-full text-black bg-white"
+                onChange={handleSearchChange}
+              />
             </div>
+            <button className="btn w-3/12 color-red">Search</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ServicesBanner;
